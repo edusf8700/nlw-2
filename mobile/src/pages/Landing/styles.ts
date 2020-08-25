ab863457c95cff5eaec5d6d9afe92f2dede3c265
Eduardo Freitas <edusf8700@gmail.com>
2020-08-25 14:07:27 -0300

    Iniciando aplicação mobile

---
+++ mobile/src/pages/Landing/styles.ts
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#8257E5',
+    flex: 1,
+    justifyContent: 'center',
+    padding: 40,
+  },
+
+  banner: {
+    width: '100%',
+    resizeMode: 'contain',
+  },
+
+  title: {
+    color: '#fff',
+    fontSize: 20,
+    lineHeight: 30,
+    marginTop: 80,
+  },
+
+  titleBold: {
+    fontWeight: 'bold'
+  },
+});
+
+export default styles;
